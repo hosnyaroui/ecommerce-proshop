a6a8a43c31be49d696ef5717a2dff87df54edbbf
hosnyaroui <hosny.cris@gmail.com>
2023-08-01 11:29:51 +0100

    Adding Rating Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,22 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container} from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
-          <LinkContainer to='/'>
+<Container>
+<LinkContainer to='/'>
             <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
-              <LinkContainer to='/cart'>
+    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+    <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='ms-auto'>
+        
+            
+           <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
                 </Nav.Link>
@@ -30,9 +26,9 @@ const Header = () => {
                   <FaUser /> Sign In
                 </Nav.Link>
               </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        </Nav>
+    </Navbar.Collapse>
+</Container>
       </Navbar>
     </header>
   )
