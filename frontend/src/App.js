c2d8a7211b6dea82fd6a33f0841df3738479c686
hosnyaroui <hosny.cris@gmail.com>
2023-08-01 10:21:53 +0100

    Listing Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <>
     <Header />
     <main className="py-3">
-    <Container><h1>welcome</h1></Container>
+    <Container>
+      <HomeScreen />
+    </Container>
 
     </main>
     <Footer />
