f93be89ed0f0798542030dedcfae4035fdec26d3
hosnyaroui <hosny.cris@gmail.com>
2023-08-01 09:40:43 +0100

    creating Header and Footer

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+    <Header />
+    <main className="py-3">
+    <Container><h1>welcome</h1></Container>
+
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
