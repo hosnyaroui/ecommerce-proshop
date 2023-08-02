34b3e6bd272848ada29909c6e42287258a78743d
hosnyaroui <hosny.cris@gmail.com>
2023-08-02 16:28:42 +0100

    Product Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,11 @@ import express from 'express';
 const router = express.Router();
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
-
+import {
+    getProducts,
+    getProductById,
+  } from '../controllers/productController.js';
+  
 router.get(
   '/',
   asyncHandler(async (req, res) => {
@@ -22,5 +26,8 @@ router.get(
     throw new Error('Resource not found');
   })
 );
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
