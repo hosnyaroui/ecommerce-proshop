d708efe3d55899c4c2dc3b88ea2116a6ef1d53b4
hosnyaroui <hosny.cris@gmail.com>
2023-08-02 17:53:42 +0100

    Redux store and API Slice

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL =
+  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '/';
+export const PRODUCTS_URL = '/api/products';
+export const USERS_URL = '/api/products';
+export const ORDERSS_URL = '/api/products';
+export const PAYPAL_URL = '/api/products';
